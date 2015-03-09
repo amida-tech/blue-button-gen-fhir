f2b6082b61929c40709ce78689af8ac2d912b7e9
Afsin Ustundag <afsin.ustundag@us.pwc.com>
2015-03-09 09:58:52 -0400

    travis + doc + test update

--- test/test-regenerate.js
+++ test/test-regenerate.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 var path = require('path');
 var chai = require('chai');
 var bbfhir = require('blue-button-fhir');
+var _ = require('lodash');
 
 var m2fhir = require('../index');
 
@@ -16,27 +17,59 @@ describe('gen fhir->gen ccda->gen fhir-> gen ccda', function () {
         return newFileName;
     };
 
+    var replaceReferences = function (obj, ids) {
+        if (obj && _.isObject(obj)) {
+            if (obj.reference && _.isString(obj.reference)) {
+                var newId = ids[obj.reference];
+                if (newId) {
+                    obj.reference = newId;
+                }
+            } else {
+                Object.keys(obj).forEach(function (key) {
+                    replaceReferences(obj[key], ids);
+                });
+            }
+        }
+    };
+
+    var makeIdsDeterministic = exports.makeIdsDeterministic = function (bundle) {
+        var ids = {};
+        bundle.entry.forEach(function (bundleEntry, index) {
+            var id = bundleEntry.id;
+            var newId = bundleEntry.content.resourceType + '/' + index.toString();
+            ids[id] = newId;
+            bundleEntry.id = newId;
+        });
+        bundle.entry.forEach(function (bundleEntry) {
+            replaceReferences(bundleEntry.content, ids);
+        });
+    };
+
     var testContent = function (fileName, outId1, outId2, patient, externalize) {
         return function () {
             var filePath = path.join(rootDir, fileName);
             var content = fs.readFileSync(filePath, 'utf8');
             var bundle = m2fhir.contentToFHIR(content, patient, externalize);
             expect(bundle).to.exist();
-            var bundle1FileName = path.join(outputDir, genModelFileName(fileName, '_bundle' + outId1));
-            fs.writeFileSync(bundle1FileName, JSON.stringify(bundle, undefined, 4));
             var model1 = bbfhir.toModel(bundle);
             expect(model1).to.exist();
             var model1FileName = path.join(outputDir, genModelFileName(fileName, outId1));
             fs.writeFileSync(model1FileName, JSON.stringify(model1, undefined, 4));
             var bundle2 = m2fhir.modelToFHIR(model1, patient, externalize);
             expect(bundle2).to.exist();
-            var bundle2FileName = path.join(outputDir, genModelFileName(fileName, '_bundle' + outId2));
-            fs.writeFileSync(bundle2FileName, JSON.stringify(bundle2, undefined, 4));
             var model2 = bbfhir.toModel(bundle2);
             expect(model2).to.exist();
             var model2FileName = path.join(outputDir, genModelFileName(fileName, outId2));
             fs.writeFileSync(model2FileName, JSON.stringify(model2, undefined, 4));
             expect(model1).to.deep.equal(model2);
+
+            makeIdsDeterministic(bundle);
+            var bundle1FileName = path.join(outputDir, genModelFileName(fileName, '_bundle' + outId1));
+            fs.writeFileSync(bundle1FileName, JSON.stringify(bundle, undefined, 4));
+
+            makeIdsDeterministic(bundle2);
+            var bundle2FileName = path.join(outputDir, genModelFileName(fileName, '_bundle' + outId2));
+            fs.writeFileSync(bundle2FileName, JSON.stringify(bundle2, undefined, 4));
         };
     };
 
